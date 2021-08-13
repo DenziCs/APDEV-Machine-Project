14246c172aaef2de80fc1e78fe59d5de5d7363a1
PuffyThePuff <79883134+PuffyThePuff@users.noreply.github.com>
2021-08-13 21:23:37 +0800

    Moved changes to prefab

--- APDEV MP - Fisticuffs/Assets/Scripts/PlayerCollisionDetection.cs
+++ APDEV MP - Fisticuffs/Assets/Scripts/PlayerCollisionDetection.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CollisionDetection : MonoBehaviour
+public class PlayerCollisionDetection : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
