3a2c4c984490d32b666098b65e4e0be5cd8a90dc
PuffyThePuff <79883134+PuffyThePuff@users.noreply.github.com>
2021-08-14 15:04:00 +0800

    Added player auto fire

--- APDEV MP - Fisticuffs/Assets/Scripts/AutoFire.cs
+++ APDEV MP - Fisticuffs/Assets/Scripts/AutoFire.cs
@@ -4,15 +4,48 @@ using UnityEngine;
 
 public class AutoFire : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject barrel;
+    [SerializeField] private GameObject bulletTemplate;
+
+    public List<GameObject> bulletPool;
+    private float timer = 0f;
+    private float totalTime = 0f;
+
+    //lower = faster
+    public float fireRate = 1f;
+
+    private void SpawnBullet(GameObject templateObject, Vector3 pos)
     {
-        
+        GameObject bullet = GameObject.Instantiate(templateObject, null);
+        bullet.SetActive(true);
+        bullet.transform.position = pos;
+        this.bulletPool.Add(bullet);
+    }
+
+    public void DespawnBullet(GameObject gameObject)
+    {
+        for (int i = 0; i < this.bulletPool.Count; i++)
+        {
+            if (this.bulletPool[i] == gameObject)
+            {
+                this.bulletPool.RemoveAt(i);
+                break;
+            }
+        }
+        GameObject.Destroy(gameObject);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 pos = barrel.transform.position;
+
+        this.totalTime += Time.deltaTime;
+        this.timer += Time.deltaTime;
+        if (this.timer >= this.fireRate)
+        {
+            this.timer = 0f;
+            SpawnBullet(bulletTemplate, pos);
+        }
     }
 }
